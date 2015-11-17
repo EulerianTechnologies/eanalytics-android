69aaea3e09ab2d31eadd6b5d6b57728dbc5be1c3
François Rouault <francois.rouault.fr@gmail.com>
2015-11-17 14:15:47 +0100

    v1.5

--- eanalytics/src/main/java/com/eulerian/android/sdk/Config.java
+++ eanalytics/src/main/java/com/eulerian/android/sdk/Config.java
@@ -5,7 +5,7 @@ package com.eulerian.android.sdk;
  */
 class Config {
 
-    static final String SDK_VERSION = "1.4.2";
+    static final String SDK_VERSION = "1.5";
 
     static final long MAX_UNZIPPED_BYTES_PER_SEND = 100000;
     static final long POST_FAILED_RETRY_DELAY_MILLIS = 5000;
